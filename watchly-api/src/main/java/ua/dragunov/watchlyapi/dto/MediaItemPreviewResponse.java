39384495aac160f446cdd3d06ffbb7d3115391c9
Igor Dragunov <igor.dragunov99@gmail.com>
2025-08-22 23:38:38 +0300

    feat(business logic): add MediaItemSearchRequest, MediaItemPreviewResponse, MediaItemSpecifications classes and add search method in MediaItem with filtering

---
+++ watchly-api/src/main/java/ua/dragunov/watchlyapi/dto/MediaItemPreviewResponse.java
@@ -0,0 +1,9 @@
+package ua.dragunov.watchlyapi.dto;
+
+public record MediaItemPreviewResponse(
+        Long id,
+        String title,
+        int releaseYear,
+        String poster,
+        int rating
+) {}
---
+++ watchly-api/src/main/java/ua/dragunov/watchlyapi/repository/MediaItemSpecifications.java
@@ -0,0 +1,31 @@
+package ua.dragunov.watchlyapi.repository;
+
+import org.springframework.data.jpa.domain.Specification;
+import ua.dragunov.watchlyapi.model.MediaItem;
+import ua.dragunov.watchlyapi.model.MediaType;
+
+import java.util.List;
+
+public class MediaItemSpecifications {
+
+    public static Specification<MediaItem> titleContains(String title) {
+        return (root, query, cb) ->
+                title == null || title.isBlank() ? null :
+                        cb.like(cb.lower(root.get("title")), "%" + title.toLowerCase() + "%");
+    }
+
+    public static Specification<MediaItem> hasType(MediaType type) {
+        return (root, query, cb) -> type == null ? null : cb.equal(root.get("type"), type);
+    }
+
+    public static Specification<MediaItem> releasedIn(Integer year) {
+        return (root, query, cb) -> year == null ? null : cb.equal(root.get("releaseYear"), year);
+    }
+
+    public static Specification<MediaItem> hasGenres(List<String> genres) {
+        return (root, query, cb) -> {
+            if (genres == null || genres.isEmpty()) return null;
+            return root.join("genres").get("name").in(genres);
+        };
+    }
+}
